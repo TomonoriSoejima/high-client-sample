d16f68fdda1d795c479d387d70d3940e228f49d9
TomonoriSoejima <tomo@elastic.co>
2018-06-18 14:34:34 +0900

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,18 +2,33 @@ import org.apache.http.HttpHost;
 import org.elasticsearch.action.DocWriteResponse;
 import org.elasticsearch.action.index.IndexRequest;
 import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.action.search.SearchRequest;
+import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.action.support.replication.ReplicationResponse;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.QueryBuilders;
+import org.elasticsearch.search.SearchHit;
+import org.elasticsearch.search.SearchHits;
+import org.elasticsearch.search.builder.SearchSourceBuilder;
 
+import java.util.ArrayList;
 import java.util.Date;
+import java.util.List;
+import java.util.Map;
 
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
+
+        sample_code();
+        System.exit(0);
+
+        List kore = new ArrayList();
+        kore.add("hello");
 
 
         RestHighLevelClient client = new RestHighLevelClient(
@@ -51,9 +66,52 @@ public class Main {
             }
         }
 
+
+
         System.exit(0);
 
 
+    }
+
+    public static void sample_code() throws Exception
+    {
+        RestHighLevelClient client = new RestHighLevelClient(
+                RestClient.builder(
+                        new HttpHost("localhost", 9200, "http"),
+                        new HttpHost("localhost", 9201, "http")));
+
+        SearchRequest searchRequest = new SearchRequest("test_index");
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+        searchSourceBuilder.query(QueryBuilders.matchAllQuery());
+        searchRequest.source(searchSourceBuilder);
+        SearchResponse searchResponse;
+        searchResponse = client.search(searchRequest);
+        SearchHits hits = searchResponse.getHits();
+
+
+
+        List<sb_object>  sb_objects = new ArrayList<sb_object>();
+        SearchHit[] searchHits = hits.getHits();
+        for (SearchHit hit : searchHits) {
+            // do something with the SearchHit
+            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+            sb_object sb = new sb_object();
+            sb.setMessageid( (String) sourceAsMap.get("message-id"));
+            sb.setMessageUID( (String) sourceAsMap.get("messageUID"));
+            sb.setCmd( (String) sourceAsMap.get("cmd"));
+            sb.setMsgid( (String) sourceAsMap.get("msgid"));
+            sb.setSid( (String) sourceAsMap.get("sid"));
+
+            sb_objects.add(sb);
+        }
+
+        int aa = 33;
+
+    }
+
+    public static void do_something(String input)
+    {
+        System.out.println(input);
     }
 
 }
---
+++ src/main/java/sb_object.java
@@ -0,0 +1,52 @@
+public class sb_object {
+
+    public String messageid;
+    public String messageUID;
+    public String cmd;
+    public String msgid;
+    public String sid;
+
+
+    public String getMessageid() {
+        return messageid;
+    }
+
+    public void setMessageid(String messageid) {
+        this.messageid = messageid;
+    }
+
+    public String getMessageUID() {
+        return messageUID;
+    }
+
+    public void setMessageUID(String messageUID) {
+        this.messageUID = messageUID;
+    }
+
+
+    public String getCmd() {
+        return cmd;
+    }
+
+    public void setCmd(String cmd) {
+        this.cmd = cmd;
+    }
+
+    public String getMsgid() {
+        return msgid;
+    }
+
+    public void setMsgid(String msgid) {
+        this.msgid = msgid;
+    }
+
+    public String getSid() {
+        return sid;
+    }
+
+    public void setSid(String sid) {
+        this.sid = sid;
+    }
+
+
+}

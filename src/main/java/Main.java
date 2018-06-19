12728a89815362aea206a2347774e656f8f8ba06
TomonoriSoejima <tomo@elastic.co>
2018-06-19 16:08:01 +0900

    commit

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -14,6 +14,7 @@ import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.elasticsearch.common.xcontent.XContentBuilder;
 import org.elasticsearch.common.xcontent.XContentFactory;
+import org.elasticsearch.index.query.BoolQueryBuilder;
 import org.elasticsearch.index.query.MatchQueryBuilder;
 import org.elasticsearch.index.query.QueryBuilder;
 import org.elasticsearch.index.query.QueryBuilders;
@@ -31,6 +32,11 @@ public class Main {
 
     static final String test_index = "test_index";
 
+    static final RestHighLevelClient client = new RestHighLevelClient(
+            RestClient.builder(
+                    new HttpHost("localhost", 9200, "http"),
+                    new HttpHost("localhost", 9201, "http")));
+
     public static void main(String[] args) throws Exception {
 
         sample_code();
@@ -40,10 +46,7 @@ public class Main {
         kore.add("hello");
 
 
-        RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("localhost", 9200, "http"),
-                        new HttpHost("localhost", 9201, "http")));
+
 
         XContentBuilder builder = XContentFactory.jsonBuilder();
         builder.startObject();
@@ -84,10 +87,7 @@ public class Main {
 
     public static void sample_code() throws Exception
     {
-        RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("localhost", 9200, "http"),
-                        new HttpHost("localhost", 9201, "http")));
+
 
         SearchRequest searchRequest = new SearchRequest(test_index);
         SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
@@ -99,12 +99,16 @@ public class Main {
 
 
         // key messageUID, value message-id
-        Map<String, String> message_id_UID = new HashMap<String, String>();
-        List<sb_object>  sb_objects = new LinkedList<sb_object>();
+        // this is a list of document that contains non null messageUID
+
+        List<sb_object> sb_objects = new LinkedList<sb_object>();
+        List<sb_object> parent_data = new LinkedList<sb_object>();
+
+
         SearchHit[] searchHits = hits.getHits();
         for (SearchHit hit : searchHits) {
             // do something with the SearchHit
-            Map<String, Object> sourceAsMap = hit.getSourceAsMap();
+                Map<String, Object> sourceAsMap = hit.getSourceAsMap();
             sb_object sb = new sb_object();
             sb.setDocid( hit.getId());
             sb.setMessageUID( (String) sourceAsMap.get("messageUID"));
@@ -119,7 +123,7 @@ public class Main {
 
             if (! sb.getMessageUID().startsWith("-") )
             {
-                message_id_UID.put(sb.getMessageUID(), sb.getMessageid());
+                parent_data.add(sb);
             }
         }
 
@@ -127,18 +131,28 @@ public class Main {
         List<String> doc_ids;
 
         // find document with same messageUID
-        for (String messageUID : message_id_UID.keySet())
+        for (sb_object sb : parent_data)
         {
 
 
-            System.out.println("searching by messageUID " + messageUID);
+            System.out.println("searching by messageUID " + sb.getMessageUID());
 
-            doc_ids = search_by_messageUID(messageUID);
+            doc_ids = search_by_messageUID(sb.getMessageUID());
             for (String id_for_update : doc_ids ) {
                 System.out.println("Found " + id_for_update);
 
             }
-            update_message_id(message_id_UID.get(messageUID), doc_ids);
+
+
+            update_message_id(sb.getMessageid(), doc_ids);
+
+
+            doc_ids = shori4(sb);
+            for (String id_for_update : doc_ids ) {
+                System.out.println("Found " + id_for_update);
+
+            }
+            update_message_id(sb.getMessageid(), doc_ids);
 
 
         }
@@ -146,17 +160,12 @@ public class Main {
 
     }
 
+    // shori 2
     // returns doc id who needs to be updated on message-id
     public static List<String> search_by_messageUID(String input) throws Exception
     {
 
-        List<String> doc_ids  = new LinkedList<String>();
-        RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("localhost", 9200, "http"),
-                        new HttpHost("localhost", 9201, "http")));
-
-//        SearchRequest searchRequest = new SearchRequest("test_index");
+        List<String> doc_ids  = new LinkedList<>();
 
         SearchSourceBuilder sourceBuilder = new SearchSourceBuilder();
         sourceBuilder.query(QueryBuilders.termQuery("cmd", input));
@@ -164,14 +173,7 @@ public class Main {
         SearchRequest searchRequest = new SearchRequest();
         searchRequest.indices(test_index);
         searchRequest.source(sourceBuilder);
-//        QueryBuilder matchQueryBuilder = QueryBuilders.matchQuery("cmd", input);
 
-
-
-//        MatchQueryBuilder matchQueryBuilder = new MatchQueryBuilder("cmd", input);
-        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
-//        searchSourceBuilder.query(sourceBuilder);
-//        searchSourceBuilder.query(matchQueryBuilder);
         SearchResponse searchResponse;
         searchResponse = client.search(searchRequest);
         SearchHits hits = searchResponse.getHits();
@@ -187,19 +189,13 @@ public class Main {
     }
 
 
+    // shori 3
     public static void update_message_id(String message_id, List docids) throws Exception {
 
+        // making dummy header to accommodate  to this change.
+        // https://www.elastic.co/guide/en/elasticsearch/reference/master/breaking_70_restclient_changes.html
+        Header header = new BasicHeader("a", "a");
 
-        Header header = new BasicHeader("a", "ba");
-
-//        headers.add(new BasicHeader("User-Agent",userAgent));
-        // create client
-
-
-        RestHighLevelClient client = new RestHighLevelClient(
-                RestClient.builder(
-                        new HttpHost("localhost", 9200, "http"),
-                        new HttpHost("localhost", 9201, "http")));
 
             for (Object docid : docids)
             {
@@ -211,11 +207,49 @@ public class Main {
 
 
                 UpdateResponse updateResponse = client.update(request, header);
+                System.out.println("UPDATED : message-id " + message_id + " on doc " + docid.toString());
 
 
             }
 
-            int aa = 9;
+    }
+
+//     - 処理４
+//    取得したmessage-idに対して、msgidが同じ値のドキュメントを特定する
+//    public static void shor4
+    public static List<String> shori4(sb_object sb) throws Exception{
+
+
+        List<String> doc_ids  = new LinkedList<>();
+
+
+        SearchSourceBuilder searchSourceBuilder = new SearchSourceBuilder();
+
+        QueryBuilder termQueryBuilder = QueryBuilders.termQuery("msgid", sb.getMsgid());
+        QueryBuilder termQueryBuilder2 = QueryBuilders.termQuery("message-id", "-");
+
+
+        BoolQueryBuilder qb = new BoolQueryBuilder();
+        qb.filter(termQueryBuilder).filter(termQueryBuilder2);
+
+        searchSourceBuilder.query(qb);
+
+        SearchRequest searchRequest = new SearchRequest();
+        searchRequest.indices(test_index);
+        searchRequest.source(searchSourceBuilder);
+
+
+        SearchResponse searchResponse;
+        searchResponse = client.search(searchRequest);
+        SearchHits hits = searchResponse.getHits();
+
+        SearchHit[] searchHits = hits.getHits();
+        for (SearchHit hit : searchHits) {
+            doc_ids.add((hit.getId()));
+        }
+
+        return doc_ids;
+
     }
 
 
